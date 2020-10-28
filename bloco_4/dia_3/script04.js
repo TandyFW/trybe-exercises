af884dd5a08bc00793dc28c9e9046a434a091e8d
Tandy <tandyfw@gmail.com>
2020-10-28 10:31:28 -0300

    Resolvido exercicio 4

--- bloco_4/dia_3/script04.js
+++ bloco_4/dia_3/script04.js
@@ -1,26 +1,26 @@
 // 04. Depois, faça uma pirâmide com n asteriscos de base:
 
-let n = 7;
-let controleEspaco = n/2;
+let n = 20;
+let controleEspaco = Math.floor(n/2);
 let space = "";
 
 
 if(n % 2 == 0){
   let asterisco = "**";
   for(let coluna = 0; coluna < n; coluna += 2){
-    for(let linha = 2; linha < controleEspaco; linha += 2){
+    for(let linha = 1; linha < controleEspaco; linha += 1){
       space += " ";
     }
     console.log(space + asterisco + space);
     asterisco += "**";
-    controleEspaco /= 2;
+    controleEspaco -= 1;
     space = "";
   }
 } else{
   let asterisco = "*";
   for(let coluna = 0; coluna < n; coluna += 2){
     for(let linha = 0; linha < controleEspaco; linha += 1){
-      space += "1";
+      space += " ";
     }
     console.log(space + asterisco + space);
     asterisco += "**";
