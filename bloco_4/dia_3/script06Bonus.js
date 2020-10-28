145f704e28dcc81f1842118c3948a78d674e8295
Tandy <tandyfw@gmail.com>
2020-10-28 10:54:11 -0300

    Resoluçao exercicio 6 bonus

---
+++ bloco_4/dia_3/script06Bonus.js
@@ -0,0 +1,13 @@
+let n = 29;
+let check = 0;
+
+for(let pos = 1; pos <= n; pos += 1){
+  if(n % pos == 0){
+    check += 1;
+  }
+}
+if(check == 2){
+  console.log(n + " é um número primo");
+} else{
+  console.log(`${n} não é um número primo`);
+}
