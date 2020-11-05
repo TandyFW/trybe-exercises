595ac614285e6759ae96cef861aa6915ebf26b4f
Tandy <tandyfw@gmail.com>
2020-11-05 11:22:36 -0300

    Questoes 1 e 2 concluidas, questao 3 em andamento

--- bloco_5/dia_3/script.js
+++ bloco_5/dia_3/script.js
@@ -1,5 +1,5 @@
 function createDaysOfTheWeek() {
-  const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+  const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado'];
   const weekDaysList = document.querySelector('.week-days');
 
   for (let index = 0; index < weekDays.length; index += 1) {
@@ -14,3 +14,76 @@ function createDaysOfTheWeek() {
 createDaysOfTheWeek();
 
 // Escreva seu código abaixo.
+/* 
+01. O array dezDaysList contém os dois últimos dias de novembro e os dias do mês de dezembro.
+Desenvolva uma função que crie dinamicamente cada dia do calendário e os adicione como filhos/filhas
+da tag <ul> com ID "days". Note que os dias 29 e 30 de novembro estão no array pois representam respectivamente Segunda-feira e Terça-feira.
+
+Os dias devem estar contidos em uma tag <li>, e todos devem ter a classe day. Ex: <li class="day">3</li>
+Os dias 24, 25 e 31 são feriados e, além da classe day, devem conter também a classe holiday. Ex: <li class="day holiday">24</li>
+Os dias 4, 11, 18 e 25 são Sexta-feira. Eles devem conter a classe day e a classe friday. Ex: <li class="day friday">4</li>
+*/
+
+const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+function dezDays () {
+  let ulDays = document.querySelector('#days');
+
+  for (let index = 0; index < dezDaysList.length; index += 1) {
+    let day = dezDaysList[index];
+    let liDays = document.createElement('li');
+    if (day === 24 || day === 31) {
+      liDays.className = 'day holiday';
+      liDays.innerHTML = day;
+      ulDays.appendChild(liDays);
+    } else if (day === 4 || day === 11 || day === 18) {
+      liDays.className = 'day friday';
+      liDays.innerHTML = day;
+      ulDays.appendChild(liDays);
+    } else if (day === 25) {
+      liDays.className = 'day holiday friday';
+      liDays.innerHTML = day;
+      ulDays.appendChild(liDays);
+    } else {
+    liDays.className = 'day';
+    liDays.innerHTML = day;
+    ulDays.appendChild(liDays);
+    }
+  }
+}
+dezDays();
+
+/*
+02. Implemente uma função que receba como parâmetro a string "Feriados" e crie dinamicamente um botão com o nome "Feriados".
+Adicione a este botão a ID "btn-holiday".
+Adicione este botão como filho/filha da tag <div> com classe "buttons-container".
+*/
+
+function btnFeriados(string) {
+  let div = document.querySelector('.buttons-container');
+  let btn = document.createElement('button');
+  btn.textContent = string;
+  btn.id = 'btn-holiday';
+  div.appendChild(btn);
+}
+btnFeriados('Feriados');
+
+/*
+03. Implemente uma função que adicione ao botão "Feriados" um evento de "click" que muda a cor de fundo dos dias que possuem a classe "holiday".
+É interessante que este botão possua também a lógica inversa. Ao ser clicado novamente ele retorna à configuração inicial com a cor "rgb(238,238,238)".
+*/
+
+function btnEventHolidays () {
+  let btn = document.querySelector('#btn-holiday');
+  let holidays = document.querySelectorAll('.holiday');
+  btn.addEventListener('click', function(){
+    for (let index = 0; index < holidays.length; index += 1) {
+      if (holidays[index].style.backgroundColor = "rgb(238 , 238 , 238)") { 
+      holidays[index].style.backgroundColor = "lightgreen";
+    } else {
+      holidays[index].style.backgroundColor = "rgb(238 , 238 , 238)";
+    };
+    };
+  });
+}
+btnEventHolidays();
