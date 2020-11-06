a8a8132515cb2a7e22467d747c474c276dec679f
Tandy <tandyfw@gmail.com>
2020-11-05 21:10:58 -0300

    Questao 8 concluida

--- bloco_5/dia_3/script.js
+++ bloco_5/dia_3/script.js
@@ -157,3 +157,18 @@ function newTask (newTask) {
   task.appendChild(span);
 }
 newTask("Codar");
+
+/*
+08. Implemente uma função que adiciona uma legenda com cor para a tarefa criada no exercício anterior.
+Esta função deverá receber como parâmetro uma string ("cor") e criar dinamicamente um elemento de tag <div> com a classe task.
+O parâmetro cor deverá ser utilizado como cor de fundo da <div> criada.
+O elemento criado deverá ser adicionado como filho/filha da tag <div> que possui a classe "my-tasks".
+*/
+
+function legenda(cor) {
+  div = document.querySelector('.my-tasks');
+  legend = document.createElement('div');
+  legend.style.backgroundColor = cor;
+  div.appendChild(legend);  
+}
+legenda("red");
