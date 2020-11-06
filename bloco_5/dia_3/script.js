5b1f6e812e43820c3cade01c848addffeb976e40
Tandy <tandyfw@gmail.com>
2020-11-06 10:17:11 -0300

    Questao Bonus concluida

--- bloco_5/dia_3/script.js
+++ bloco_5/dia_3/script.js
@@ -214,3 +214,36 @@ function dayTask() {
 }
 
 dayTask();
+
+/*
+Bônus: Vamos adicionar compromissos ao seu calendário? Implemente uma função que, ao digitar um compromisso
+na caixa de texto "COMPROMISSOS", adiciona o item à lista "MEUS COMPROMISSOS" ao clicar no botão "ADICIONAR".
+Se nenhum caractere for inserido no campo input, a função deve retornar um alert com uma mensagem de erro ao clicar em "ADICIONAR".
+Ao pressionar a tecla "enter" o evento também deverá ser disparado.
+Dica - Propriedade: keyCode.
+*/
+
+function addCompromissos() {
+  let taskList = document.querySelector('.task-list');
+  let btnComp = document.querySelector('#btn-add');
+  let taskbox = document.querySelector('#task-input');
+  btnComp.addEventListener('click', addComp);
+  taskbox.addEventListener('keypress', function(event) {
+    if (event.keyCode === 13) {
+      addComp();
+    }
+  });
+  function addComp (){
+    let texto = document.querySelector('#task-input').value;
+    if(texto === "") {
+      alert("Nenhum compromisso digitado!");
+    } else {
+      let compromisso = document.createElement('li');
+      compromisso.innerText = texto;
+      taskList.appendChild(compromisso);
+      console.log(texto);
+    }
+  }
+}
+
+addCompromissos();
