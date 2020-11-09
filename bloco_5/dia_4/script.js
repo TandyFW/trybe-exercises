9d9e3975b83b7bf39a8dd90f0bed62a0409a7381
Tandy <tandyfw@gmail.com>
2020-11-09 09:49:07 -0300

    Exercicio concluido

--- bloco_5/dia_4/script.js
+++ bloco_5/dia_4/script.js
@@ -1,6 +1,9 @@
 window.onload = function() {
   document.body.style.backgroundColor = localStorage.getItem('bgColor');
   document.body.style.color = localStorage.getItem('fontColor');
+  document.body.style.fontSize = localStorage.getItem('fontSize');
+  document.body.style.lineHeight = localStorage.getItem('lineHeight');
+  document.body.style.fontFamily = localStorage.getItem('fontFamily');
 }
 
 function btnEvents () {
@@ -18,6 +21,25 @@ function btnEvents () {
     localStorage.setItem('fontColor', fontColor);
   });
 
-  
+  let btnfontSize = document.querySelector('#btn-fontSize');
+  btnfontSize.addEventListener('click', function() {
+    let fontSize = document.querySelector('#fontSize').value;
+    document.body.style.fontSize = fontSize + 'px';
+    localStorage.setItem('fontSize', fontSize + 'px');
+  });
+
+  let btnlineHeight = document.querySelector('#btn-lineHeight');
+  btnlineHeight.addEventListener('click', function() {
+    let lineHeight = document.querySelector('#lineHeight').value;
+    document.body.style.lineHeight = lineHeight;
+    localStorage.setItem('lineHeight', lineHeight);
+  });
+
+  let btnfontFamily = document.querySelector('#btn-fontFamily');
+  btnfontFamily.addEventListener('click', function() {
+    let fontFamily = document.querySelector('#fontFamily').value;
+    document.body.style.fontFamily = fontFamily;
+    localStorage.setItem('fontFamily', fontFamily);
+  });
 }
 btnEvents();
