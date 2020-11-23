bc74a236fb51ae9a3a6109682af422a9225e5275
Tandy <tandyfw@gmail.com>
2020-11-23 15:38:16 -0300

    Exercicio parte 1.1 concluido

--- bloco_7/dia_1/parte_1.js
+++ bloco_7/dia_1/parte_1.js
@@ -4,16 +4,15 @@ Modifique a estrutura da função para que ela seja uma arrow function.
 Modifique as concatenações para template literals.
 Copie o código abaixo.
 */
-function testingScope(escopo) { 
+const testingScope = escopo => { 
   if (escopo === true) { 
-    var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
     ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
     console.log(ifScope);
   } else {
-    var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+    const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
     console.log(elseScope);
   }
-  console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
 }
 
 testingScope(true);
