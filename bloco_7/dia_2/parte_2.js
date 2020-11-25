c953393a403528a8e9bc75bf637c9d61f7533e36
Tandy <tandyfw@gmail.com>
2020-11-25 10:33:51 -0300

    Parte 2.7 concluida

--- bloco_7/dia_2/parte_2.js
+++ bloco_7/dia_2/parte_2.js
@@ -68,5 +68,14 @@ const alunos = () => lesson1.numeroEstudantes + lesson2.numeroEstudantes + lesso
 console.log(alunos());
 
 /*
-7. Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto. Por exemplo:
+7. Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto.
 */
+
+const valorPorChave = (objeto, chave) => objeto[Object.keys(objeto)[chave]];
+console.log(valorPorChave(lesson1, 0));
+
+/*
+8. Crie uma função que verifique se o par (chave / valor) existe na função.
+Essa função deve possuir três parâmetros, sendo eles: o objeto, o nome da chave e o valor da chave.
+*/
+
