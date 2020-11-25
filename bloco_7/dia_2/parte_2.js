daa31b724172b47cf7ad2b1709c81976ab6aa95c
Tandy <tandyfw@gmail.com>
2020-11-25 10:28:19 -0300

    Parte 2.6 concluida

--- bloco_7/dia_2/parte_2.js
+++ bloco_7/dia_2/parte_2.js
@@ -60,3 +60,13 @@ const allLessons = {
   lesson3: Object.assign({}, lesson3)
 };
 console.log(allLessons);
+
+/*
+6. Usando o objeto criado no exercício 5, crie uma função que retorne o número total de estudantes em todas as aulas.
+*/
+const alunos = () => lesson1.numeroEstudantes + lesson2.numeroEstudantes + lesson3.numeroEstudantes;
+console.log(alunos());
+
+/*
+7. Crie uma função que obtenha o valor da chave de acordo com a sua posição no objeto. Por exemplo:
+*/
