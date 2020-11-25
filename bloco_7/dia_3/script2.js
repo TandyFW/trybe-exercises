0e59d8a2e81208696d679be86d6891c0742a5921
Tandy <tandyfw@gmail.com>
2020-11-25 18:25:46 -0300

    Exercicio 1 concluido

---
+++ bloco_7/dia_3/script2.js
@@ -0,0 +1,15 @@
+const assert = require('assert');
+
+function myRemove(arr, item) {
+  let newArr = [];
+  for (let i = 0; i < arr.length; i += 1) {
+    if (item !== arr[i]) {
+      newArr.push(arr[i]);
+    }
+  }
+  return newArr;
+}
+
+// implemente seus testes aqui
+
+// 1. Verifique se a chamada myRemove([1, 2, 3, 4], 3) retorna o array esperado
