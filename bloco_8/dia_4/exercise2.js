ab110fa2efa40b2a030531089fe7550f86ce4cd1
Tandy <tandyfw@gmail.com>
2020-12-03 20:13:07 -0300

    Exercicio 2 concluido

--- bloco_8/dia_4/exercise2.js
+++ bloco_8/dia_4/exercise2.js
@@ -1,7 +1,14 @@
-const criarElemento = (...args) => {
-  const element = document.createElement(args[0]);
-  element.id = args[1];
-  element.className = args[2];
-};
+/*
+Crie uma função sum que dado um número variável de elementos retorna a soma desses elementos. Ou seja:
+Dica: use parâmetro rest.
+*/
+const assert = require('assert')
 
-console.log(criarElemento('p', 'paragrafo', 'paragrafo'));
+const sum = (...numbers) => numbers.reduce((accumulate, current) => accumulate + current, 0);
+
+
+assert.strictEqual(sum(), 0)
+assert.strictEqual(sum(1), 1)
+assert.strictEqual(sum(1, 2), 3)
+assert.strictEqual(sum(1, 2, 3), 6)
+assert.strictEqual(sum(1, 2, 3, 4), 10)
