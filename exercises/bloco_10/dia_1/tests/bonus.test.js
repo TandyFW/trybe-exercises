f42194c001fcbdf91617c163e7eac3a8df2d85b2
TandyFW <tandyfw@gmail.com>
2021-01-12 13:00:57 -0300

    Exercicio bonus em conclusão

---
+++ exercises/bloco_10/dia_1/tests/bonus.test.js
@@ -0,0 +1,19 @@
+const searchEmployee = require('../bonus');
+
+describe('Testando funcionalidades da função searchEmployee', () => {
+  it('Testa se searchEmployee é uma função.', () => {
+    expect('function').toBe(typeof(searchEmployee));
+  });
+  it('Testa se a função retorna o primeiro nome do(a) funcionário(a) pesquisado(a) pelo id', () => {
+    expect('Linda').toBe(searchEmployee('1256-4', 'firstName'));
+  });
+  it('Testa se a função retorna o último nome do(a) funcionário(a) pesquisado(a) pelo id', () => {
+    expect('Dodds').toBe(searchEmployee('4678-2', 'lastName'));
+  });
+  it('Testa se a função retorna as especialidades do(a) funcionário(a) pesquisado(a) pelo id', () => {
+    expect(['UX', 'Design']).toEqual(searchEmployee('8579-6', 'specialities'));
+  });
+  it('Testa se a função retorna o Error ID não identificada caso não encontre um id', () => {
+    expect(() => searchEmployee('1111-1', 'firstName').toThrow('ID não identificada'));
+  });
+});
--- exercises/bloco_10/dia_1/tests/sum.test.js
+++ exercises/bloco_10/dia_1/tests/sum.test.js
@@ -10,5 +10,5 @@ test('Teste se a função sum lança um erro quando os parametros são 4 e "5" (
 });
 
 test('Teste se a mensagem de erro é "parameters must be numbers" quando realizar a chamada sum(4, "5")', () => {
-  expect(() => sum(4, "5").toThrow('parameters must be numbers'));
+  expect(() => sum(4, "5").toThrowError(new Error('parameters must be numberslll')));
 });
